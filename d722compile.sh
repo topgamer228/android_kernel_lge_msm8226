0c691c7836a3bccf3737af187c3f71f21dbf33d8
Darren Ramen <darren.ramen@gmail.com>
2015-12-16 18:33:19 +0400

    Create d722compile.sh

---
+++ d722compile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+$model=D722
+
+make1="make mrproper"
+
+make2="make jagnm_cyanogenmod_defconfig"
+
+if [ -e "./arch/arm/boot/dt.img" ]; then
+rm ./arch/arm/boot/dt.img
+fi
+
+if [ -e "./arch/arm/boot/msm8226-v1-jagnm.dtb" ]; then
+rm ./arch/arm/boot/msm8226-v1-jagnm.dtb
+rm ./arch/arm/boot/msm8226-v2-jagnm.dtb
+fi
+
+$make1 && $make2 && make -j5 && ./dtbToolCM -2 -s 2048 -p ./scripts/dtc/ -o ./arch/arm/boot/dt.img ./arch/arm/boot/
+
+if [ ! -d "Output" ]; then
+mkdir Output
+fi
+
+echo "Copying files to respective folder"
+
+		cd ./RAMDISK/$model/
+		./cleanup.sh
+		./unpackimg.sh boot.img
+		cp ../boot.img-ramdiskcomp ./split_img/boot.img-ramdiskcomp
+		cp ../../arch/arm/boot/zImage ./split_img/boot.img-zImage
+		cp ../../arch/arm/boot/dt.img ./split_img/boot.img-dtb
+		echo "Repacking Kernel"
+		./repackimg.sh
+		echo "Signing Kernel"
+		./bump.py image-new.img
+		cd ../../
+		echo "Moving Kernel to output folder"
+		mv ./RAMDISK/$model/image-new_bumped.img ./Output/$model.img
+
