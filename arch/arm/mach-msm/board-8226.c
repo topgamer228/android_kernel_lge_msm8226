f83f2fba779a1768c7a085a4dc035cfd6f9711cf
OdintTech3 <nickspirit3+gitHub@gmail.com>
2016-08-07 21:25:08 +0300

    intellithermal v2: adapt for MSM8226 architecture from Spirit Kernel for Moto G
    (cherry picked from commit 8acd103da970278725b0231b4a6cf2101acb81b1)

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -145,6 +145,9 @@ void __init msm8226_add_drivers(void)
 	fan53555_regulator_init();
 	cpr_regulator_init();
 	tsens_tm_init_driver();
+	#ifdef CONFIG_INTELLI_THERMAL
+		msm_thermal_device_init();
+	#endif
 	msm_thermal_device_init();
 }
 
