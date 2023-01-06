50c6897cfdbf0a1b211a4421f1b21b30ddd33eb9
topgamer228 <olegtrololololo@gmail.com>
2023-01-06 23:06:33 +0300

    Use python2 for gcc_wrapper.py

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
@@ -53,7 +53,7 @@ def interpret_warning(line):
     line = line.rstrip('\n')
     m = warning_re.match(line)
     if m and m.group(2) not in allowed_warnings:
-        print("error, forbidden warning:", m.group(2))
+        print "error, forbidden warning:", m.group(2)
 
         # If there is a warning, remove any object if it exists.
         if ofile:
@@ -78,17 +78,17 @@ def run_gcc():
     try:
         proc = subprocess.Popen(args, stderr=subprocess.PIPE)
         for line in proc.stderr:
-            print(line, end=' ')
+            print line,
             interpret_warning(line)
 
         result = proc.wait()
     except OSError as e:
         result = e.errno
         if result == errno.ENOENT:
-            print(args[0] + ':',e.strerror)
-            print('Is your PATH set correctly?')
+            print args[0] + ':',e.strerror
+            print 'Is your PATH set correctly?'
         else:
-            print(' '.join(args), str(e))
+            print ' '.join(args), str(e)
 
     return result
 
